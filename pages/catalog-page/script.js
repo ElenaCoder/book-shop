d38d6ba8fa2f6259374033e58e4c7ceb1c5a53a7
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2022-11-18 18:36:03 +0200

    feat: add modal windows to cards

--- pages/catalog-page/script.js
+++ pages/catalog-page/script.js
@@ -54,6 +54,7 @@ for(let i=0; i < 10; i++){
     // const h1Elem = document.createElement("h1");
 
     const divCard = document.createElement("div");
+    divCard.id = `card-${i}`;
     divCard.className = "card";
     // divContainer.append(divCard);
 
@@ -74,6 +75,11 @@ for(let i=0; i < 10; i++){
     pAuthor.className ="author";
     divCardContentContainer.append(pAuthor);
 
+    const cardCloseButton = document.createElement("button");
+    cardCloseButton.className = "card-close-button";
+    cardCloseButton.innerHTML = "&times;";
+    divCardContentContainer.append(cardCloseButton);
+
     const divTitle = document.createElement("div");
     divTitle.className ="title";
     divCardContentContainer.append(divTitle);
@@ -85,13 +91,46 @@ for(let i=0; i < 10; i++){
 
     const aShowMore = document.createElement("a");
     aShowMore.className = "show-more";
-    aShowMore.href = "#";
+    // aShowMore.href = "#modal-8";
+    aShowMore.href = `#modal-${i}`;
     aShowMore.textContent = "Show more...";
+    aShowMore.setAttribute("data-modal-target",".modal");
     divCardContentContainer.append(aShowMore);
 
-    const divDescription = document.createElement("div");
-    divDescription.className = "description";
-    divCardContentContainer.append(divDescription);
+    /*--------Modal wondow----------- */
+    const divModal = document.createElement("div");
+    divModal.className = "modal";
+    divModal.id = `#modal-${i}`;
+    divCardContentContainer.append(divModal);
+
+    const divModalHeader = document.createElement("div");
+    divModalHeader.className = "modal-header";
+    divModal.append(divModalHeader);
+
+    const divModalTitle = document.createElement("div");
+    divModalTitle.className = "modal-title";
+    divModalHeader.append(divModalTitle);
+
+    const divModalButton = document.createElement("button");
+    divModalButton.className = "close-button";
+    divModalButton.setAttribute("data-close-button", "");
+    divModalButton.innerHTML = "&times;";
+    divModalHeader.append(divModalButton);
+
+    const divModalBody = document.createElement("div");
+    divModalBody.className = "modal-body";
+    divModalBody.innerHTML = "modal-body-modal-body-modal-body-modal-bodymodal-body";
+    divModal.append(divModalBody);
+
+    const divModalOverlay = document.createElement("div");
+    divModalOverlay.className = "overlay";
+    divModal.after(divModalOverlay);
+
+    /*// -------Modal wondow ------------*/
+
+    // const divDescription = document.createElement("div");
+    // divDescription.className = "description";
+    // divCardContentContainer.append(divDescription);
 
     const divCardButton = document.createElement("div");
     divCardButton.className = "card-button";
@@ -132,14 +171,65 @@ let books = fetch(bookListURL, config) //path to the file with json data
                 fragmentCardArr[index].querySelector(".author").innerHTML = book.author;
                 fragmentCardArr[index].querySelector(".title").innerHTML  = book.title;
                 fragmentCardArr[index].querySelector(".price").innerHTML  += book.price;
-                fragmentCardArr[index].querySelector(".description").innerHTML  = book.description;
-            })
 
+                fragmentCardArr[index].querySelector(".modal-title").innerHTML  = book.title;
+                fragmentCardArr[index].querySelector(".modal-body").innerHTML  = book.description;
+                // fragmentCardArr[index].querySelector(".description").innerHTML  = book.description;
+
+
+
+            })
 
             console.log("fragmentCardArr", fragmentCardArr[0].querySelector(".author").innerHTML);
 
 
             fragmentCardArr.forEach((elem, index) => h1Elem.after(elem));
+
+
+             /* JS for Module Window */
+             const openModalButtons = document.querySelectorAll("[data-modal-target]");
+             const closeModalButtons = document.querySelectorAll("[data-close-button]");
+             const overlay = document.querySelector(".overlay");
+
+             openModalButtons.forEach(function(button,index) {
+                 button.addEventListener("click", () => {
+                     const modal = document.querySelectorAll(button.dataset.modalTarget)[index];
+                     openModal(modal);
+                 })
+             })
+
+             overlay.addEventListener("click", () => {
+                 const modals = document.querySelectorAll(".modal.active");
+                 modals.forEach(modal => {
+                     closeModal(modal);
+                 });
+             });
+
+             closeModalButtons.forEach(button => {
+                 button.addEventListener("click", () => {
+                    const modal = button.closest(".modal");
+                    closeModal(modal);
+                 })
+             })
+
+             function openModal(modal) {
+                 if(modal === null) return
+                 modal.classList.add("active");
+                 overlay.classList.add("active");
+
+
+             }
+
+             function closeModal(modal) {
+                 if(modal === null) return
+                 modal.classList.remove("active");
+                 overlay.classList.remove("active");
+             }
+
+
+             /* //JS for Module Window */
+
+
 
 
         });
